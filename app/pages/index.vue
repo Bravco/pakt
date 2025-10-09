f01cf1940185ae828477745c2d96d3841e526219
Bravco <boss.wiw@gmail.com>
2025-10-09 12:06:12 +0200

    Started doing the hero section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,28 @@
 <template>
-    <div>
-    </div>
+    <UPageHero
+        title="A sea of opportunities"
+        headline="pakt the deal."
+        :links="links"
+    >
+        <template #description>
+            <p>Connect with the perfect collaboration partners.<br>Build authentic relationships. Create campaigns that drive real results.</p>
+        </template>
+    </UPageHero>
 </template>
+
+<script setup lang="ts">
+    const links = ref([
+        {
+            label: "Get started",
+            to: "/",
+            icon: "i-lucide-handshake"
+        },
+        {
+            label: "Learn More",
+            to: "/",
+            trailingIcon: "i-lucide-corner-right-down",
+            color: "neutral",
+            variant: "subtle"
+        }
+    ]);
+</script>
