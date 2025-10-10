b155e7c3471c2feefb793bf397a0bd3c59a93d76
Bravco <boss.wiw@gmail.com>
2025-10-10 11:36:24 +0200

    Added waitlist CTA

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,25 +13,23 @@
                 variant="link"
                 color="neutral"
                 class="underline underline-offset-2 cursor-pointer"
+                :to="{ path: '/', hash: '#waitlist' }"
             />
         </template>
         <template #title>
-            <h1>A sea of <span class="text-primary">opportunities</span></h1>
+            A sea of <span class="text-primary">opportunities</span>
         </template>
         <template #description>
             <p>Connect with the perfect collaboration partners.<br>Build authentic relationships. Create campaigns that drive real results.</p>
         </template>
     </UPageHero>
 
-    <UPageCTA title="Join the waitlist" description="Get latest updates on pakt and be the first one to try it out!" variant="subtle">
+    <UPageCTA id="waitlist" title="Join the waitlist" description="Get latest updates on pakt and be the first one to try it out!" variant="subtle">
         <template #links>
             <UFormField name="email" size="lg">
-                <UInput type="email" placeholder="your@email.com" class="w-full">
-                    <template #trailing>
-                        <UButton type="submit" size="xs" color="neutral" label="Subscribe" class="cursor-pointer"/>
-                    </template>
-                </UInput>
+                <UInput type="email" placeholder="your@email.com"/>
             </UFormField>
+            <UButton type="submit" label="Submit" class="cursor-pointer"/>
         </template>
     </UPageCTA>
 </template>
