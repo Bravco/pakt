2304f4854ec388b7b2e9fbe8370a3c104f7face6
Bravco <boss.wiw@gmail.com>
2025-10-11 18:09:04 +0200

    Changed secondary color

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,14 +2,14 @@ export default defineAppConfig({
     ui: {
         colors: {
             primary: "sky",
-            secondary: "yellow",
+            secondary: "amber",
             neutral: "zinc"
         },
         button: {
             compoundVariants: [
                 {
                     variant: "solid",
-                    class: "hover:bg-secondary-300 hover:text-neutral-900 active:bg-secondary-500 dark:active:bg-secondary/50 focus-visible:bg-secondary-300 focus-visible:text-neutral-900"
+                    class: "hover:bg-secondary active:bg-secondary focus-visible:bg-secondary"
                 }
             ]
         }
