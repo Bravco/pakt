7a727da047ecc54907666440c57b5ef1199837e6
Bravco <boss.wiw@gmail.com>
2025-10-11 20:13:43 +0200

    Changed the color mode preference to light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
-  css: ["~/assets/main.css"]
-})
+  css: ["~/assets/main.css"],
+  colorMode: {
+    preference: "light"
+  }
+});
